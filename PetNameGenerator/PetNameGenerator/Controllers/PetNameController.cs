5bc5ce33e7d9440c559438a516864a4198eac7e0
DesierreLacerona <DesierreLacerona@SMU-0109>
2025-03-26 15:56:29 +0800

    petnames

---
+++ PetNameGenerator/PetNameGenerator/Controllers/PetNameController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace PetNameGenerator.Controllers
+{
+    [ApiController]
+    [Route("api/petnames")]
+    public class PetNameController : ControllerBase
+    {
+        private static readonly Random rnd = new Random();
+
+        private static readonly string[] dogNames = { "Buddy", "Max", "Charlie", "Rocky", "Rex" };
+        private static readonly string[] catNames = { "Whiskers", "Mittens", "Luna", "Simba", "Tiger" };
+        private static readonly string[] birdNames = { "Tweety", "Sky", "Chirpy", "Raven", "Sunny" };
+
+        [HttpPost("generate")]
+        public IActionResult GenerateName([FromBody] PetNameRequest request)
+        {
+            if (request == null || string.IsNullOrEmpty(request.AnimalType))
+            {
+                return BadRequest(new { error = "The 'animalType' field is required." });
+            }
+
+            string animalType = request.AnimalType.ToLower();
+            string[] nameList;
+
+            if (animalType == "dog")
+            {
+                nameList = dogNames;
+            }
+            else if (animalType == "cat")
+            {
+                nameList = catNames;
+            }
+            else if (animalType == "bird")
+            {
+                nameList = birdNames;
+            }
+            else
+            {
+                return BadRequest(new { error = "Invalid animal type. Allowed values: dog, cat, bird." });
+            }
+
+            if (request.TwoPart.HasValue && !(request.TwoPart is bool))
+            {
+                return BadRequest(new { error = "The 'twoPart' field must be a boolean (true or false)." });
+            }
+
+            string petName;
+
+            if (request.TwoPart == true)
+            {
+                petName = nameList[rnd.Next(nameList.Length)] + nameList[rnd.Next(nameList.Length)];
+            }
+            else
+            {
+                petName = nameList[rnd.Next(nameList.Length)];
+            }
+
+            return Ok(new { name = petName });
+        }
+    }
+
+    public class PetNameRequest
+    {
+        public string AnimalType { get; set; }
+        public bool? TwoPart { get; set; }
+    }
+}
